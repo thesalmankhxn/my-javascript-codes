38042062219b396025a4ee203877e49fb0b04fc5
iamSK77 <sk77.mobilefix@gmail.com>
2020-12-20 09:53:06 +0530

    added performance func

--- string_reverse/str_reverse.js
+++ string_reverse/str_reverse.js
@@ -1,9 +1,11 @@
+const { performance } = require('perf_hooks');
+
 function reverseStr(str) {
 
     // return str.split("").reverse().join("");
 
     let final = "";
-    for (let i = str.length - 1; i >= 0; i--){
+    for (let i = str.length - 1; i >= 0; i--) {
         final += str[i];
     }
     return final;
@@ -11,4 +13,8 @@ function reverseStr(str) {
 
 let s = "SK77 is King";
 
+let t1 = performance.now();
 console.log(reverseStr(s));
+let t2 = performance.now();
+
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
