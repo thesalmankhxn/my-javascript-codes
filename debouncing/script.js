0a6576f7c4a928db34ede101e0c2601f01445545
iamSK77 <sk77.mobilefix@gmail.com>
2021-01-03 23:14:48 +0530

    debouncing

---
+++ debouncing/script.js
@@ -0,0 +1,16 @@
+// Debouncing
+
+let counter = 0;
+const getData = () => {
+
+    // calls an API and gets Data
+    console.log('Fetching Data', counter++);
+};
+
+const debounce = function (fn, d) {
+    return function () {
+
+    };
+};
+
+const betterGetData = debounce(getData, 300);
