063421071f135f6c9740e7070baa59f24cf9bbb4
iamSK77 <sk77.mobilefix@gmail.com>
2021-05-22 21:58:45 +0530

    update: add factorial

--- factorialise_a_number/factorialise.js
+++ factorialise_a_number/factorialise.js
@@ -2,9 +2,8 @@ const factorial = s => {
     let num = 1;
     for (let i = 1; i <= s; i++) {
         num *= i;
-        console.log(num, i);
     }
     return num;
 };
 
-console.log(factorial(2));
+console.log(factorial(5));
