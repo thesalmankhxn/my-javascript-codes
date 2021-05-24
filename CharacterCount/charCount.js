2a579daadb52b18bb6f5f5e0bfe1a5a95ef7e81f
iamSK77 <sk77.mobilefix@gmail.com>
2021-05-24 23:15:42 +0530

    program: char count

---
+++ CharacterCount/charCount.js
@@ -0,0 +1,10 @@
+const charCount = str => {
+    let allCharCount = {};
+
+    for (let i = 0; i <= str.length; i++) {
+        console.log(str[i])
+    }
+};
+
+const string = 'This is a String';
+console.log(charCount(string));
