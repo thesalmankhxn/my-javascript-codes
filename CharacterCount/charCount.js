4d020409096c17583a27bf32a1e0ba016da26d18
iamSK77 <sk77.mobilefix@gmail.com>
2021-05-27 23:18:23 +0530

    fix: add checks for not counting the empty spaces

--- CharacterCount/charCount.js
+++ CharacterCount/charCount.js
@@ -4,14 +4,16 @@ const charCount = str => {
     for (let i = 0; i < str.length; i++) {
         let char = str[i].toLowerCase();
 
-        if (result[char] > 0) {
-            result[char]++;
-        } else {
-            result[char] = 1;
-        };
+        if (char != ' ') {
+            if (result[char] > 0) {
+                result[char]++;
+            } else {
+                result[char] = 1;
+            };
+        }
     }
 
     return result;
 }
 
-console.log(charCount('Hello'));
+console.log(charCount('SK77 is KING'));
